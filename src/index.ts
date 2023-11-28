2ee6720a0751b354ecc7cd1f59a2dfdf7181a818
Katerina <kate.clysm@gmail.com>
2023-11-28 15:10:21 -0300

    Auth Context

--- src/index.ts
+++ src/index.ts
@@ -3,10 +3,10 @@ import express from 'express';
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello, Express with TypeScript!');
-});
+// app.get('/', (req, res) => {
+//   res.send('Hello, Express with TypeScript!');
+// });
 
 app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+  console.log(`The server of Midnight Mysteries is working! running in port:  ${port}`);
 });
